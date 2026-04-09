9ffe07524dddc1e3c53c93ec13f7f908921089ca
unknown <sonct2k3@gmail.com>
2026-04-09 23:13:44 +0700

    save before pull

--- api/repositories/Interfaces/IProductRepository.cs
+++ api/repositories/Interfaces/IProductRepository.cs
@@ -4,16 +4,18 @@ using System.Linq;
 using System.Threading.Tasks;
 using api.DTOs.product;
 using api.models.entities;
+using api.repositories.implementations;
 
-namespace api.repositories.Interfaces
+namespace api.repositories.interfaces
 {
-    public interface IProductRepository
+    public interface IProductRepository : IRepository<Product>
     {
-        Task<(List<Product> Items, int Total)> GetPageAsync(ProductQueryDto query);
-        Task<Product?> GetByIdAsync(string id);
+        Task<(List<Product> Items, int Total)> GetPagedAsync(ProductQueryDto query);
+        Task<Product?> GetByIdWithIncludesAsync(string id);
         Task<Product?> GetBySlugAsync(string slug);
         Task<bool> ExistsSkuAsync(string sku, string? excludeId = null);
         Task<bool> ExistsSlugAsync(string slug, string? excludeId = null);
+        Task SyncCategoriesAsync(string productId, List<string> categoryIds);
 
     }
 }
